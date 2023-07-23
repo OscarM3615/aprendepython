5b08cdb69b19bf6e153a2d78fe97d2a79a3864a1
Óscar Miranda <oscarmiranda3615@gmail.com>
2023-07-23 13:14:05 -0600

    feat(utils): allow custom title in selection

--- aprendepython/utils.py
+++ aprendepython/utils.py
@@ -1,15 +1,28 @@
-from typing import Sequence
+from typing import Optional, Sequence
 from rich.console import Console
 
 console = Console(style='blue', width=80)
 
 
-def selection(options: Sequence[str]) -> int:
-    console.print(
-        '[white]Selecciona el número de la opción y presiona Enter.[/]\n'
-    )
+def selection(options: Sequence[str], *, title: Optional[str] = None) -> int:
+    """
+    Generate a styled menu with the provided options and return the selected
+    option as an int.
+
+    Args:
+        options (Sequence[str]): The options to include in the menu.
+        title (Optional[str]): An optional, customised title.
+
+    Returns:
+        int: The index of the selected option (starting in 1).
+    """
+
+    if not title:
+        title = 'Selecciona el número de la opción y presiona Enter.'
+    console.print(f'[white]{title}[/]\n')
+
     for i, opt in enumerate(options, start=1):
-        console.print(f'[white]{i}. {opt}[/]')
+        console.print(f'[white]{i}: {opt}[/]')
 
     # Repeat the input until the answer is a number.
     while True:
