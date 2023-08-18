1fc7126e384484552d9fcf80fded4eb4e3dc664e
Óscar Miranda <oscarmiranda3615@gmail.com>
2023-08-18 15:38:04 -0600

    feat(content): add missing lessons for data types

--- aprendepython/content/__init__.py
+++ aprendepython/content/__init__.py
@@ -3,9 +3,13 @@ from ..models.lesson import Lesson
 
 from .lesson_00 import lesson_00
 from .lesson_01 import lesson_01
+from .lesson_02 import lesson_02
+from .lesson_03 import lesson_03
 
 
 lessons: List[Lesson] = [
     lesson_00,
     lesson_01,
+    lesson_02,
+    lesson_03,
 ]
---
+++ aprendepython/content/lesson_03.py
@@ -0,0 +1,38 @@
+import re
+
+from ..models.lesson import Lesson
+from ..models.exercise import OnelinerExercise
+from .lesson_02 import lesson_02
+
+
+class DeclareBoolVariable(OnelinerExercise):
+    instructions = ('A continuación declara una variable llamada "soy_mayor" '
+    'con una valor True o False, dependiendo de si eres mayor de edad o no.')
+    hints = [
+        'Recuerda asignar el nombre correcto a la variable.',
+        'Los valores lógicos solo pueden ser True o False.'
+    ]
+
+    def test(self) -> bool:
+        return bool(re.match(r'^soy_mayor *?= *?(True|False)', self.source))
+
+
+lesson_03 = Lesson(
+    'tipo-de-dato-logico',
+    'Tipo de dato lógico',
+    [
+        'Además de variables numéricas y de texto, el lenguaje Python cuenta '
+        'el tipo de variable lógico. Este tipo de variable acepta solamente '
+        'dos valores: True y False.\n\n'
+        'Este tipo de dato se puede utilizar cuando solamente se tienen '
+        'opciones binarias, por ejemplo: encendido/apagado, '
+        'activado/desactivado, permitido/prohibido, etc.',
+
+        'En este ejemplo se muestra la declaración de variables lógicas:\n\n'
+        'curso_de_python = True\n'
+        'python_es_dificil = False',
+
+        DeclareBoolVariable(),
+    ],
+    lesson_02
+)
